b536078a2b82d80060ee9b8ff7a0852258b91e69
Prospertech-github <ikechukwuprosper8@gmail.com>
2024-10-30 22:29:40 +0100

    FEAT: Added Products fetching from the API

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,13 +1,45 @@
 import Navbar from '../../components/Navbar'
 import DarkBG from '../../components/DarkBG'
 import Footer from '../../components/Footer'
+import ProductCard from '../../components/ProductCard'
 import styles from './Shop.module.css' 
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Shop() {
+  const [products, setProducts] = useState([])
+  const [groceries, setGroceries] = useState(false)
+  const [skinCare, setSkinCare] = useState([])
+  useEffect(()=>{
+    axios.get('http://localhost:8080/api/v1/products?limit=20').then(response => {setProducts(response.data.data.products)})
+    // fetch('')
+  },[])
+
+  useEffect(()=>{
+    axios.get()
+  })
   return (
     <>
         <Navbar />
-        <DarkBG />
+        <DarkBG text="Shop">
+          <div>
+            <p onClick={()=>setGroceries(!groceries)}>Groceries</p> 
+            <p> &#62; </p>
+            <p onClick={()=>setSkinCare(!skinCare)}>Skin Care</p>
+          </div> 
+        </DarkBG>
+          <section className={styles.productArea}>
+            {products.map((product)=>{
+              return(
+                <ProductCard productName={product.title} category={product.category.title} price={`$${product.price}`} imgUrl={product.icon} key={products.id}/>
+              )
+            })}
+
+            {/* {
+              groceries && 
+            } */}
+
+          </section>
         <Footer />
     </>
   )
